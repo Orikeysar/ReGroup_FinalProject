555f3fdce85462377f8910a36b2c85ecc297257d
Your Name <116570267+Orikeysar@users.noreply.github.com>
2023-03-04 19:50:18 +0200

    סידור עריכת תמונה

--- src/Hooks/useAuthStatus.js
+++ src/Hooks/useAuthStatus.js
@@ -6,12 +6,16 @@ export const useAuthStatus = () => {
   const [logedIn, setLogedIn] = useState(false);
   const [loading, setLoading] = useState(true);
   const isUserSignIn = useRef(true)
+  const [activeUser, setActiveUser] = useState(() => {
+    const user = JSON.parse(localStorage.getItem("activeUser"));
+    return user;
+  });
   useEffect(() => {
     if(isUserSignIn){
 
       const auth = getAuth();
     onAuthStateChanged(auth, (user) => {
-      if (user) {
+      if (user && activeUser ) {
         setLogedIn(true);
       }
       setLoading(false);
