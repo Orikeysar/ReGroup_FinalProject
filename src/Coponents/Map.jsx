7c432d04cec862f51ed876350341aa91e00b35a7
galbb6 <116492049+galbb6@users.noreply.github.com>
2023-03-09 13:33:54 +0200

    בעיה בדרופדאון של הסינונים

--- src/Coponents/Map.jsx
+++ src/Coponents/Map.jsx
@@ -11,48 +11,63 @@ import {
   query,
 } from "firebase/firestore";
 
-export default function Map() {
+export default function Map({ filteredGroups }) {
   // החזרת המפה כשהמרכז שלה ( ברירת מחדל ) היא רופין ובתוכה של הסימניות שנרנדר דינמי מהדאטה
-  const [activeGroups, setActiveGroups] = useState([]);
+  // const [activeGroups, setActiveGroups] = useState([]);
+  // const [filteredGroups, setFilteredGroups] = useState(activeGroups);
+
   const { isLoaded } = useLoadScript({
     googleMapsApiKey: "AIzaSyCt1tGfbI6o0A6dcCFTstFsPlAUEQYaYS4",
   });
-  
+
   if (!isLoaded) return <Spinner />;
 
-  const colRef = collection(db, "activeGroups");
-  const q = query(colRef);
+  // const colRef = collection(db, "activeGroups");
+  // const q = query(colRef);
 
-  onSnapshot(q, (snapshot) => {
-    let newActiveGroups = [];
-    snapshot.docs.forEach((doc, index) => {
-      newActiveGroups.push({ ...doc.data(), id: doc.id, index });
-    });
-    if (JSON.stringify(newActiveGroups) !== JSON.stringify(activeGroups)) {
-      setActiveGroups(newActiveGroups);
-    }
-  });
+  // onSnapshot(q, (snapshot) => {
+  //   let newActiveGroups = [];
+  //   snapshot.docs.forEach((doc, index) => {
+  //     newActiveGroups.push({ ...doc.data(), id: doc.id, index });
+  //   });
+  //   if (JSON.stringify(newActiveGroups) !== JSON.stringify(activeGroups)) {
+  //     setActiveGroups(newActiveGroups);
+  //   }
+  // });
+
+  // const filterMarkers =()=> {
+  //   setFilteredGroups(activeGroups);
+  //   if (filtters.course) {
+  //     filteredGroups = filteredGroups.filter((group) => group.groupTittle === filtters.course);
+  //   }
+  //   if (filtters.subjects && filtters.subjects.length > 0) {
+  //     filteredGroups = filteredGroups.filter((group) => filtters.subjects.includes(group.subject));
+  //   }
+  //   if (filtters.number) {
+  //     filteredGroups = filteredGroups.filter((group) => group.groupSize === filtters.number);
+  //   }
+  //   setFilteredGroups(filteredGroups);
+  // }
+    
   
-
   return (
     <>
-       <GoogleMap
+      <GoogleMap
         zoom={16}
         center={{ lat: 32.342884, lng: 34.912755 }}
         mapContainerClassName=" map-container"
       >
-        {activeGroups.map((item) => (
+        {filteredGroups.map((item) => (
           <Marker
             key={item.index}
             title={item.groupTittle}
             position={{
               lat: item.location.latitude,
               lng: item.location.longitude,
-            }}            
+            }}
           />
         ))}
-      </GoogleMap> 
-      
+      </GoogleMap>
     </>
   );
 }
--- src/Pages/FindGroups.jsx
+++ src/Pages/FindGroups.jsx
@@ -7,13 +7,19 @@ import { db } from "../FirebaseSDK";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import Spinner from "../Coponents/Spinner";
-import RecentActivitiesCard from "../Coponents/RecentActivitiesCard";
-import CoursesList from "../Coponents/CoursesList";
 import NavBar from "../Coponents/NavBar";
 import BottumNavigation from "../Coponents/BottumNavBar";
-import FriendsListCard from "../Coponents/FriendsListCard";
-import UserAchievemeant from "../Coponents/UserAchievemeant";
 import Map from "../Coponents/Map";
+import TextField from "@mui/material/TextField";
+import Autocomplete from "@mui/material/Autocomplete";
+import Chip from "@mui/material/Chip";
+import {
+  onSnapshot,
+  collection,
+  getDocs,
+  orderBy,
+  query,
+} from "firebase/firestore";
 
 function FindGroups() {
   const navigate = useNavigate();
@@ -22,13 +28,98 @@ function FindGroups() {
     const user = JSON.parse(localStorage.getItem("activeUser"));
     return user;
   });
+  //משיכה של הדאטה בזמן אמת
+  const [activeGroups, setActiveGroups] = useState([]);
+  const [filteredGroups, setFilteredGroups] = useState([]);
+
+  const colRef = collection(db, "activeGroups");
+  const q = query(colRef);
+
+  onSnapshot(q, (snapshot) => {
+    let newActiveGroups = [];
+    snapshot.docs.forEach((doc, index) => {
+      newActiveGroups.push({ ...doc.data(), id: doc.id, index });
+    });
+    if (JSON.stringify(newActiveGroups) !== JSON.stringify(activeGroups)) {
+      setActiveGroups(newActiveGroups);
+      setFilteredGroups(newActiveGroups);
+    }
+  });
+
   const [courses, setCourses] = useState(
     JSON.parse(localStorage.getItem("courses"))
   );
-  const [selectedCourses, setSelectedCourses] = useState(null);
+
+  const [subjects, setSubjects] = useState(() => {
+    let newListSubjects = [];
+    courses.map((item) => {
+      for (let index = 0; index < item.subjects.length; index++) {
+        newListSubjects.push(item.subjects[index]);
+      }
+    });
+    return newListSubjects;
+  });
+  //המשתנה הזה מכיל את כל הנושאים השייכים לקורס שהמשתמש בחר
+  const [subjectsOfCourses, setSubjectsOfCourses] = useState(null);
+//איתחול ראשוני של החירות בפועל
+  const [selectedCourse, setSelectedCourse] = useState(null);
+  const [selectedSubjects, setSelectedSubjects] = useState(null);
+  const [selectedNumber, setSelectedNumber] = useState(null);
+  const handleCourseChange = (event, value) => {
+    setSelectedCourse(value);
+   
+    if (value) {
+      courses.forEach((element) => {
+        if (element.id === value) {
+          setSubjectsOfCourses(element.subjects);
+        }
+
+      });
+    } else {
+      setSubjectsOfCourses(null);
+    } 
+  };
+console.log(selectedCourse)
+  const handleSubjectsChange = (event, value) => {
+    setSelectedSubjects(value);
+  };
+  const handleNumberChange = (event, value) => {
+    setSelectedNumber(value);
+    
+  };
+  useEffect(()=>{
+    if(selectedCourse!==null || selectedSubjects!==null || selectedNumber!==null){
+      filterMarkers();
+
+    }
+  },[])
+  //סינון הצגת הקבוצות לפי בחירת המשתמש
+  const filterMarkers = () => {
+    let newFilter=activeGroups;
+    if (selectedCourse) {
+      newFilter = newFilter.filter(
+        (group) => group.groupTittle === selectedCourse
+      );
+    }
+    if (selectedSubjects && selectedSubjects.length > 0) {
+      newFilter = newFilter.filter((group) =>
+      selectedSubjects.includes(group.subject)
+      );
+    }
+    if (selectedNumber) {
+      newFilter = newFilter.filter(
+        (group) => group.groupSize === selectedNumber
+      );
+    }
+    setFilteredGroups(newFilter);
+  };
+
   if (activeUser == null) {
     return <Spinner />;
   }
+  
+
+
   return (
     <div className="container">
       {/* //TOP NAVBAR */}
@@ -42,12 +133,55 @@ function FindGroups() {
       </div>
       <div className=" flex justify-center mb-2">
         <label className=" text-lg">here you can find groups or </label>&nbsp;
-        <label onClick={()=>navigate("/createGroups")}  className=" font-bold text-lg hover:drop-shadow-xl underline">Create new+</label>
+        <label
+          onClick={() => navigate("/createGroups")}
+          className=" font-bold text-lg hover:drop-shadow-xl underline"
+        >
+          Create new+
+        </label>
+      </div>
+      <div className=" grid justify-center my-4">
+        <Autocomplete
+          onChange={handleCourseChange}
+          id="free-solo-demo"
+          freeSolo
+          sx={{ width: 250 }}
+          options={courses.map((option) => option.id)}
+          renderInput={(params) => <TextField {...params} label="Course" />}
+        />
+        <Autocomplete
+          className=" my-5"
+          onChange={handleSubjectsChange}
+          multiple
+          id="tags-filled"
+          options={subjectsOfCourses ? subjectsOfCourses : subjects}
+          freeSolo
+          renderTags={(value, getTagProps) =>
+            value.map((option, index) => (
+              <Chip
+                variant="outlined"
+                label={option}
+                {...getTagProps({ index })}
+              />
+            ))
+          }
+          renderInput={(params) => (
+            <TextField {...params} label="Subjects" placeholder="Favorites" />
+          )}
+        />
+        <Autocomplete
+          onChange={handleNumberChange}
+          id="free-solo-demo"
+          freeSolo
+          sx={{ width: 250 }}
+          options={["2", "3", "4", "5"]}
+          renderInput={(params) => <TextField {...params} label="Group size" />}
+        />
       </div>
       <div className=" p-1 drop-shadow-xl">
-        <Map/>
+        <Map filteredGroups={filteredGroups} />
       </div>
-     
+
       <div className="buttomNavBar w-full  absolute bottom-0 pb-4">
         <BottumNavigation />
       </div>
