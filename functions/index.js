2329cf3347d32d83ca76557ca879bd9ab8989e07
galbb6 <116492049+galbb6@users.noreply.github.com>
2023-04-03 18:56:41 +0300

    add function

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,8 @@ const functions = require("firebase-functions");
 
 const admin = require("firebase-admin");
 admin.initializeApp();
-
+const FCM_TOKEN_COLLECTION = "fcmTokens";
+const FCM_TOKEN_KEY = "fcmToken";
 // runs every day at 00:00 am and reset the actionsNuumber for users
 exports.scheduledFunction = functions.pubsub
     .schedule("0 0 * * *")
@@ -30,3 +31,27 @@ exports.scheduledFunction = functions.pubsub
       await Promise.all(promises);
       console.log("Actions number reset complete");
     });
+
+// Define the function that will send message
+exports.onButtonClick = functions.https.onCall(async (data, context) => {
+  // Get the user ID from the client-side
+  const userId = data.userId;
+  const documentSnapshot = await admin.firestore()
+      .collection(FCM_TOKEN_COLLECTION).doc(userId).get();
+  const token = documentSnapshot.data()[FCM_TOKEN_KEY];
+  const message = "ITS WORKS!!!";
+  const payload = {
+    token,
+    notification: {
+      title: "ReGroup",
+      body: message,
+    },
+  };
+
+  admin.messaging().send(payload).then((response) => {
+    // Response is a message ID string.
+    functions.logger.log("Successfully sent message: ", response);
+  }).catch((error) => {
+    functions.logger.log("error: ", error);
+  });
+});
