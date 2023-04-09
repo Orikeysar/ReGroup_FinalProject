8cceace22c8578c85482e16f585a2e906cbeda71
galbb6 <116492049+galbb6@users.noreply.github.com>
2023-04-09 21:54:56 +0300

    Update index.js

--- functions/index.js
+++ functions/index.js
@@ -2,7 +2,6 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 const nodemailer = require("nodemailer");
-const cors = require("cors")({origin: true});
 // runs every day at 00:00 am and reset the actionsNuumber for users
 exports.scheduledFunction = functions.pubsub
     .schedule("0 0 * * *")
@@ -78,28 +77,27 @@ exports.alertGroupAdded = functions.https.onCall(async (data, context) => {
 });
 // Create a transporter object using SMTP
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
   auth: {
-    user: functions.config().gmail.email,
-    pass: functions.config().gmail.password,
+    user: "regroup.info.emails@gmail.com",
+    pass: "utujrywqkzbptzet",
   },
 });
 // Create a function to send email
-exports.sendEmail = functions.https.onCall(async (data, context) => {
-  cors(context.req, context.res, async () => {
-    const {name, email, message} = data;
-
-    // Set up the email message
-    const mailOptions = {
-      from: functions.config().gmail.email,
-      to: email,
-      subject: `Message from ${name}`,
-      text: message,
-    };
-
-    // Send the email using Nodemailer
-    await transporter.sendMail(mailOptions);
-
-    return {message: "Email sent successfully!"};
+exports.sendMailOverHTTP = functions.https.onRequest((req, res) => {
+  const mailOptions = {
+    from: "regroup.info.emails@gmail.com",
+    to: "naorori1212@gmail.com",
+    subject: "contact form message",
+    html: "<h1>Order Confirmation</h1>",
+  };
+  return transporter.sendMail(mailOptions, (error, data) => {
+    if (error) {
+      return res.send(error.toString());
+    }
+    const data1 = JSON.stringify(data);
+    return res.send(`Sent! ${data1}`);
   });
 });
